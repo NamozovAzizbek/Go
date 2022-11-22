ebac660ada9c04df78373ee4f9cc03ea14dbf80a
AzizBekNI <117757921+AzizBekNI@users.noreply.github.com>
2022-11-22 11:48:13 +0500

    interface 1

---
+++ interface/misol1.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type greater interface {
+	great(string) string
+}
+
+type uzb struct{}
+type eng struct{}
+
+func (u *uzb) great(name string) string {
+	return fmt.Sprintf("Salom %s", name)
+}
+func (e *eng) great(name string) string {
+	return fmt.Sprintf("Hello %s", name)
+}
+
+func sayHello (g greater, name string){
+	fmt.Print(g.great(name))
+}
+
+
+
+func main() {
+sayHello(&uzb{}, "azizbek")
+sayHello(&eng{}, "azizbek")
+
+}
